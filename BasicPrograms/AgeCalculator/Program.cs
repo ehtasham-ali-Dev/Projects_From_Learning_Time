b2bd6c2f0d130f92196a290ee8485a2bd66e81ef
Ehtasham Ali <ehtasham.dev@outlook.com>
2025-06-29 11:54:15 +0500

    Added age calculator project.

---
+++ BasicPrograms/AgeCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AgeCalculator
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //these are the programs releted to just basic concepts like arthimatic operations,simple I/O and type casting 
+            Console.WriteLine("Heye !! How are u ");
+            Console.WriteLine("What's your name ");
+            string nameOfPerson=Console.ReadLine();
+            Console.WriteLine("What's Your year of birth");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+           
+            int currentYear =DateTime.Now.Year;
+            Console.Clear();
+
+            int userAge = currentYear - birthYear; //This is a formula for age finding 
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Heye " + nameOfPerson +" Your Age is : " + userAge);
+            Console.WriteLine("----------------------");
+
+        }
+    }
+}
