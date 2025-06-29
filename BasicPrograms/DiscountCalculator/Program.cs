7c05ecced6b1d41b8a4ae022b22e328e88cd258d
Ehtasham Ali <ehtasham.dev@outlook.com>
2025-06-29 12:20:40 +0500

    Added discount Calculator app

---
+++ BasicPrograms/DiscountCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace DiscountCalculator
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = CultureInfo.CurrentCulture;
+            RegionInfo region = new RegionInfo(culture.Name);
+
+            string currencySymbol = region.CurrencySymbol; 
+            string currencyName = region.ISOCurrencySymbol;  //these are advance concepts about culture info etc but i was just familer with them that something exsists to get culture info so i applied it and get the code from internet for begning 
+
+            Console.Write("Enter product price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter discount percentage: ");
+            double discount = Convert.ToDouble(Console.ReadLine());
+
+            double discountAmount = (price * discount) / 100;
+            double finalPrice = price - discountAmount;
+
+            Console.WriteLine("Discount: " + discountAmount + currencySymbol);
+            Console.WriteLine("Final Price: " + finalPrice + currencySymbol);
+        }
+    }
+}
