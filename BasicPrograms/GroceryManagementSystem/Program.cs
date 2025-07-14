da3d06e8ddc70669f31b86161057364e908110d3
Ehtasham Ali <ehtasham.dev@outlook.com>
2025-07-14 10:54:16 +0500

    ...

---
+++ BasicPrograms/GroceryManagementSystem/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace GroceryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] addItemArray = new string[1000];   /*i'm using array with max size
+                                                        1000 then i'll update it furthur more */
+
+
+
+
+
+            Console.WriteLine("----Welcome to Sham'z store----");
+            Console.WriteLine("========== ********** ===========");
+            Home();
+
+
+        }
+        //here i'm making functions for making my program easy to manage and readable 
+        static void Home()
+        {
+            Console.WriteLine("----(Choose  from given options)----");
+            Console.WriteLine("");
+
+            Console.WriteLine("1) Add Item");
+            Console.WriteLine("2) Veiw Item");
+            Console.WriteLine("3) Remove Item");
+            Console.WriteLine("4) Exit Item");
+            int chooise=int.Parse(Console.ReadLine());
+            Chooise(chooise);
+
+
+
+        }
+        static void Chooise(int chooise)
+        {
+            switch (chooise)
+            {
+                case 1:
+                    {
+                        AddProduct();
+                    }
+                    break;
+                case 2:
+                    {
+                        DisplayProductsDetails();
+
+                    }
+                    break;
+                case 3:
+                    {
+
+                    }
+                    break;
+                case 4:
+                    {
+
+                    }
+                    break;
+            }
+
+        }
+
+        static void AddProduct()
+            {
+            Console.WriteLine("---Add item here---");
+            Console.WriteLine("Item name: ");
+            string itemName=Console.ReadLine();
+
+            Console.WriteLine("Quantity(e.g:- 1,2):  ");
+            int itemQuantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Item Price: ");
+           double ItemPrice=double.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Press any key to og back home .....");
+            Console.ReadKey();
+
+            Home();
+
+            }
+        static void DisplayProductsDetails()
+        {
+            
+        }
+
+}
+}
---
+++ BasicPrograms/StudentAttendanceTracker/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace StudentAttendanceTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---Student Attendance Tracker---");
+            Console.ResetColor();
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("Enter Number Of students: ");
+            int numberOfStudents=Convert.ToInt32(Console.ReadLine());
+
+            string[] studentName = new string[numberOfStudents];
+            char[]attendence = new char[numberOfStudents];
+
+            for (int i = 0;i< numberOfStudents; i++)
+            {
+                Console.WriteLine($"Enter name of student {i+1}: ");
+                studentName[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine("Type A for absent and type p for present: ");
+            
+            for (int i = 0; i< numberOfStudents; i++)
+            {
+                Console.WriteLine($"Is {studentName[i]} present? (P/A): ");
+                attendence[i] = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine($"{studentName[i]}-{(attendence[i] == 'p' ? "present" : "Abesent")}");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Report");
+            Console.WriteLine("");
+
+            int present = 0; int absent = 0;
+            for (int i=0; i< numberOfStudents; i++)
+            {
+                Console.WriteLine($"{studentName[i]}-{(attendence[i] == 'p' ? "present" : "Abesent")}");
+                if (attendence[i] == 'p')
+                {
+                    present++;
+                }
+                else
+                {
+                    absent++;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================");
+                Console.WriteLine("Total students: {0}",numberOfStudents);
+                Console.WriteLine("Presnt students: {0}", present);
+                Console.WriteLine("Absent students: {0}", absent);
+            
+
+        }
+    }
+}
